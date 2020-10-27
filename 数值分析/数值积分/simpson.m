a2ca419d4cfa9944ffb97228eb447d492c1f8674
hope <lucifinil006@gmail.com>
2020-10-27 20:53:09 +0800

    改错以及最后整理

--- 数值分析/数值积分/simpson.m
+++ 数值分析/数值积分/simpson.m
@@ -1,9 +1,13 @@
 clc;clear
 format long
 fun = @(x) sin(x)/x;    %函数可更改
+% fun = @(x) x/(4+x^2);
+% fun = @(x) x^(1/2);
 n = input('n= ');       %输入精度
 a = 0;                  
 b = 1;
+% a = 1;
+% b = 9;
 h = (b-a)/n;
 S_1 = 0;                %置空
 S_2 = 0;
@@ -19,5 +23,6 @@ for k = 1:n-1
     S_2 = S_2+fun(x);
 end
 S_3 = 1+fun(b);
+% S_3 = fun(a)+fun(b);
 S_n = h*(4*S_1+2*S_2+S_3)/6;
 S_n
--- 数值分析/数值积分/single_sinpson.m
+++ 数值分析/数值积分/single_sinpson.m
@@ -3,5 +3,5 @@ format long
 fun = @(x) sin(x)/x;
 a = 0;
 b = 1;
-F = (b-a)/6*(1+4*fun(a+b/2)+fun(b));
+F = (b-a)/6*(1+4*fun((a+b)/2)+fun(b));
 F
