1e4a503584e63d015d83b2eae06534798c78dd49
hope <lucifinil006@gmail.com>
2020-10-27 19:47:51 +0800

    改为七位小数

--- 数值分析/数值积分/I.m
+++ 数值分析/数值积分/I.m
@@ -2,5 +2,5 @@ clc;clear
 syms x
 fun1 = x/(4+x^2);
 fun2 = x^(1/2);
-I1 = vpa(int(fun1,0,1),8)
-I2 = vpa(int(fun2,1,9),10)        %保留八位小数
+I1 = vpa(int(fun1,0,1),7)
+I2 = vpa(int(fun2,1,9),9)        %保留七位小数
