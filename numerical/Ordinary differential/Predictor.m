d8ed2c2f3a12b80fad32953caf5f69993fec4147
hope <lucifinil006@gmail.com>
2020-11-02 18:51:04 +0800

    修改

--- numerical/Ordinary differential/Predictor.m
+++ numerical/Ordinary differential/Predictor.m
@@ -1,17 +1,14 @@
 function [y_P] = Predictor(fun,x0,y0,xn,h)
-    % 预估校正法 右端函数fun，初值x0,y0,端点xn,步长h
-    n = (xn-x0)/h;    % 区间的个数
-    x = zeros(1,n+1);
-    y_P = zeros(1,n+1);
-    y_p = zeros(1,n+1);
-    y_c = zeros(1,n+1);       % 预分配内存可以提高运行速度
-    x(1) = x0;
-    y_P(1) = y0;
-    
-    for i = 1:n
-        x(i+1) = x(i)+h;
-        y_p(i) = y_P(i)+h*fun(x(i),y_P(i));
-        y_c(i) = y_P(i)+h*fun(x(i+1),y_p(i));
-        y_P(i+1) = 1/2*(y_p(i)+y_c(i));
-    end
-    
+% 预估校正法 右端函数fun，初值x0,y0,端点xn,步长h
+n = (xn-x0)/h;    % 区间的个数
+x = zeros(1,n+1);
+y_P = zeros(1,n+1);
+x(1) = x0;
+y_P(1) = y0;
+
+for i = 1:n
+    x(i+1) = x(i)+h;
+    y_p = y_P(i)+h*fun(x(i),y_P(i));
+    y_c = y_P(i)+h*fun(x(i+1),y_p);
+    y_P(i+1) = 1/2*(y_p+y_c);
+end
--- numerical/Ordinary differential/main.m
+++ numerical/Ordinary differential/main.m
@@ -1,27 +1,28 @@
 clc;clear
 % format long
-x0 = 0;
-y0 = 1;
-xn = 1;
-h = 0.1;
-fun = @(x,y) -y+x+1;
-fun_i = @(x) x+exp(-x);
+% x0 = 0;
+% y0 = 1;
+% xn = 1;
+% h = 0.1;
+% fun = @(x,y) -y+x+1;
+% fun_i = @(x) x+exp(-x);
 
-% x0 = input('x0 = ');
-% y0 = input('y0 = ');
-% xn = input('xn = ');
-% h = input('h = ');
-% fun = input('fun = ');
-% fun_i = input('fun_i = ');
+x0 = input('x0 = ');
+y0 = input('y0 = ');
+xn = input('xn = ');
+h = input('h = ');
+fun = input('fun = ');
+fun_i = input('fun_i = ');
 
 [x,y_I] = I(fun_i,x0,y0,xn,h);    % 精确值
 [y_E] = Euler(fun,x0,y0,xn,h);    % 欧拉法
 [y_P] = Predictor(fun,x0,y0,xn,h);    % 预估校正法
+[y_R] = Runge(fun,x0,y0,xn,h);    %龙格库塔法
 
-T = [x',y_I',y_E',y_P']
+T = [x',y_I',y_E',y_P',y_R']
 
-plot(x,y_I,'k', x,y_E,'b-.',x,y_P,'r:')
+plot(x,y_I,'k', x,y_E,'b-.',x,y_P,'r:',x,y_R,'y--')
 title('各方法对比')
-legend('精确值','欧拉法','预估校证法')
+legend('精确值','欧拉法','预估校证法','龙格库塔法')
 xlabel('x')
-ylabel('y_I and y_E and y_P')
+ylabel('y_I and y_E and y_P and y_R')
