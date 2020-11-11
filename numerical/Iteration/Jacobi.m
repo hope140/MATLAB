83d2bca20e3e850728c59da2c075f8f735c30428
hope <lucifinil006@gmail.com>
2020-11-11 14:01:17 +0800

    迭代法

---
+++ numerical/Iteration/Jacobi.m
@@ -0,0 +1,20 @@
+function [x,n] = Jacobi(A,b,x0,eps)
+% Jacobi迭代法，nargin在新版本中不支持
+% if nargin == 3
+%     eps = 1.0e-6;
+% elseif nargin < 3
+%     error
+%     return
+% end
+D = diag(diag(A));  % 对角矩阵
+L = -tril(A,-1);    % 下三角阵
+U = -triu(A,1);     % 上三角阵
+B = D\(L+U);
+f = D\b;
+x = B*x0+f;
+n = 1;              % 迭代次数
+while norm(x-x0) >=  eps
+    x0 = x;
+    x = B*x0+f;
+    n = n+1;
+end
