75f3e2db373f4c9bd3c047a20c1f30b956ec34c2
hope <lucifinil006@gmail.com>
2020-11-28 11:27:19 +0800

    修改

--- OLS/LSM.m
+++ OLS/LSM.m
@@ -1,12 +1,16 @@
 function [coef,intercept]=LSM(x,y)
-m = size(x,2);  % 赋值列数
-x_avg = sum(x)/m;   % 样本均值
-Bate_1 = sum(y.*(x-x_avg));
-Bate_2 = sum(x.^2)-(sum(x)).^2/m;
-coef = Bate_1/Bate_2;    % 系数
-intercept = sum(y-coef*x)/m;   % 截距
+n = size(x,2);  % 样本数量
+x_avg = sum(x)/n;   % 样本均值
+y_avg = sum(y)/n;   % 样本均值
+B_1 = 0;
+B_2 = 0;
+for i = 1:n
+    B_1 = B_1+(x(i)-x_avg)*(y(i)-y_avg);
+    B_2 = B_2+(x(i)-x_avg)^2;
+end
+coef = B_1/B_2;    % 系数
+intercept = y_avg-coef*x_avg;   % 截距
 y_predict = coef*x+intercept;  % 预测值
-plot(x,y,'ro',x,y_predict,'b--');
+plot(x,y,'ro',x,y_predict,'b-');
 legend('Training data','Linear regression');
 end
-
